6bb1ecfabe8c042f2cda71f0a0aff2cbd8cb4a85
Ashis-007 <ashisjc@gmail.com>
2020-04-22 18:16:08 +0530

    Comment feature added

--- app.js
+++ app.js
@@ -5,22 +5,26 @@ const express = require("express"),
   path = require("path"),
   mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/yelpdb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const Campground = require("./models/campground"),
+  Comment = require("./models/comment");
+const seedDB = require("./seeds");
 
-const campgroundSchema = new mongoose.Schema({
-  name: String,
-  imageURL: String
-});
-
-const Campground = new mongoose.model("Campground", campgroundSchema);
+// seedDB();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+mongoose
+  .connect("mongodb://localhost:27017/yelpdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("===DB Connected==="));
+
+// Schema/Model
+
+// ROUTES
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
@@ -30,17 +34,22 @@ app.get("/campgrounds", (req, res) => {
     if (err) {
       console.log("Something went wrong!");
     } else {
-      res.render("campgrounds", { campgrounds: campgrounds });
+      res.render("campgrounds/campgrounds", { campgrounds });
     }
   });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// POST Route
 app.post("/campgrounds", (req, res) => {
   let name = req.body.campgroundName;
   let imageURL = req.body.campgroundImage;
   let campground = new Campground({
     name: name,
-    imageURL: imageURL
+    imageURL: imageURL,
   });
 
   campground.save((err, campground) => {
@@ -52,11 +61,49 @@ app.post("/campgrounds", (req, res) => {
     }
   });
 
-  res.redirect("/campgrounds");
+  res.redirect("campgrounds/campgrounds");
 });
 
-app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+// SHOW Route
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id)
+    .populate("comments")
+    .exec((err, campground) => {
+      if (err) {
+        return res.status(404).json({
+          error: "Could not get the campground",
+        });
+      }
+      res.render("campgrounds/show", { campground });
+    });
+});
+
+// NEW COMMENT Route
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.render("comments/new", { campground });
+  });
+});
+
+// POST COMMENT Route
+app.post("/campgrounds/:id/comments", (req, res) => {
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const comment = req.body.comment;
+    Comment.create(comment).then((comment) => {
+      campground.comments.push(comment);
+      campground.save();
+
+      res.redirect("/campgrounds/" + campground._id);
+    });
+  });
 });
 
 app.listen(5000);
